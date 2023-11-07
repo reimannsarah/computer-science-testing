1094b444b8c12aaf95b28b2f7b94e66cdce79af2
Sarah Reimann <reimannsarah45@gmail.com>
2023-11-07 09:23:12 -0700

    failing test for node children

--- __tests__/bst-node.test.js
+++ __tests__/bst-node.test.js
@@ -5,5 +5,7 @@ describe('bstNode', () => {
   test('should correctly create a node', () => {
     const node = new BSTNode(36);
     expect(node.data).toEqual(36);
+    expect(node.left).toEqual(null);
+    expect(node.right).toEqual(null);
   });
 });
